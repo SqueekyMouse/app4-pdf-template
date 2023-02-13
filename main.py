f54b6fa8ca68bc707617994e22bf6d19c072f6f6
sreejith <squeeky.mouse@gmail.com>
2023-02-13 23:40:08 +0530

    Add footers Sec24

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fpdf import FPDF
 import pandas as pd
-##commit: Add more pages for each topic Sec24
+##commit: Add footers Sec24
 pdf=FPDF(orientation='P',unit='mm',format='A4')
+pdf.set_auto_page_break(auto=False,margin=0)
 # print(type(pdf))
 
 df=pd.read_csv('topics.csv')
@@ -9,13 +10,24 @@ for index,row in df.iterrows():
     # print(index,row)
     # print(index,row['Topic'])
     pdf.add_page()  
+
+    # set header
     pdf.set_font(family='Times',style='B',size=24)
     pdf.set_text_color(100,100,100)
-    # pdf.set_text_color(254,0,0)
     pdf.cell(w=0,h=12,txt=row['Topic'],align='L',ln=1)
     pdf.line(10,22,200,22)
-    
+
+    # set footer
+    pdf.ln(265)
+    pdf.set_font(family='Times',style='I',size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0,h=0,txt=row['Topic'],align='R')
+
     for i in range(row['Pages']-1):
         pdf.add_page()
+        pdf.ln(277)
+        pdf.set_font(family='Times',style='I',size=8)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w=0,h=0,txt=row['Topic'],align='R')
 
 pdf.output('tmp/out.pdf')
